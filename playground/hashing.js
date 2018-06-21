86ca1f5ce1335186d1eb25845107e9c149564a78
Danyon Satterlee <>
2018-06-21 09:13:40 -0600

    hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,12 +1,28 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
-var data ={
-    id: 10
-};
-var token = jwt.sign(data, '123abc');
-console.log(token)
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded *** ', decoded)
+
+
+const bcrypt = require('bcryptjs');
+var password = '123abc';
+
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err,hash) => { 
+//         console.log(hash);
+//     });
+// });
+
+var hashedPassword = '$2a$10$VtwhPynGNiHm7LRLJbYQTe36frx8I0AJLf6dNEFkUZvd1ICOHn9Fu';
+
+bcrypt.compare('1', hashedPassword, (err, res)=>{
+    console.log(res)
+})
+// var data ={
+//     id: 10
+// };
+// var token = jwt.sign(data, '123abc');
+// console.log(token)
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded *** ', decoded)
 
 // var message = ' i am user number 3';
 
